066a4fd81ea34fbe11b87f71b1c2c23ebc8c607c
tsuryatej <156185735+tsuryatej@users.noreply.github.com>
2024-12-13 18:55:31 +0530

    Add files via upload

---
+++ dist/version17/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from "./chunk-5A3ZLIZZ.mjs";import"./chunk-DUDUDYL4.mjs";import"./chunk-755GYTVL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MainNavComponent } from "./main-nav/main-nav.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MainNavComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'version17';
+}
