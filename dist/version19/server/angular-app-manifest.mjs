f145b347df5354b8a459627721e7ce4ed62caa29
Frontend <tsuryatej.teegavarapu@gmail.com>
2025-03-07 18:57:30 +0530

    first commit

--- dist/version19/server/angular-app-manifest.mjs
+++ dist/version19/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 5330, hash: '9a78f57e2d7e708eb4cf6c98ec30d36de6d1a3bc71728d2cbfe023c10e266274', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1058, hash: '29a15fcdd7ba468c4aa60a91ffd575b8f02368ca6d895e93ed3723116a29827d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5520, hash: '2bf424145f74a4f89c8626f2d81b40518e7fc5d73acc7dfe54f33410626f8f42', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1248, hash: '7f6afa17f551990ba6ac07058c89ceeabd4b501f8a5483404a34b00927ba13ad', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-TCGJFIP4.css': {size: 262693, hash: 'sXPQ3K0bKxg', text: () => import('./assets-chunks/styles-TCGJFIP4_css.mjs').then(m => m.default)}
   },
 };
